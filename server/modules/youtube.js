6ecbc65136b91ddeac619b6a6e819e0e7ddbb43c
Olivier FERRIOT <of266145@iut21.u-bourgogne.fr>
2021-11-16 14:06:46 +0100

    fix: remove localhost from chat.js

---
+++ server/modules/youtube.js
@@ -0,0 +1,33 @@
+﻿/*
+ * Nom : Youtube
+ * Description : Ce module de la mort integre une recherche de video youtube
+ * Auteur(s) : Interlosers (Olivier & Yanis)
+ */
+
+// Définit les méthodes "publiques" (utilisation à l'extérieur du module)
+module.exports = 
+{
+	handleYoutube: handleYoutube // permet d'appeler cette méthode dans server.js -> youtube.handleYoutube(...)
+}
+
+/**
+ * Lorsqu'on appelle youtube, il renvoie une liste de video...
+ */
+function handleYoutube(io, message)
+{
+	// Passe le message en minuscules (recherche insensible à la casse)
+	messageMinified = message.toLowerCase();
+	// Recupere le code de la video Youtube
+	var youtubeCode = message.substring(8);
+	
+	// Est-ce qu'il contient une référence à Daffy ?
+	if (messageMinified.startsWith('yt:'))
+	{
+		// Si oui, envoie la réponse de Daffy...
+		io.sockets.emit('new_message',
+		{
+			name:'Youtube',
+			message:''
+		});
+	}
+}
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var fs = require('fs');			// Accès au système de fichier
 // Chargement des modules perso
 var daffy = require('./modules/daffy.js');
 var youtubeMini = require('./modules/youtubeMini.js');
+var youtube = require('./modules/youtube.js');
 
 // Initialisation du serveur HTTP
 var app = express();
@@ -50,6 +51,7 @@ io.sockets.on('connection', function(socket)
 		
 		// Transmet le message au module YoutubeMini (on lui passe aussi l'objet "io" pour qu'il puisse envoyer des messages)
 		youtubeMini.handleYoutubeMini(io, message);
+		youtube.handleYoutube(io, message);
 	});
 });
 
