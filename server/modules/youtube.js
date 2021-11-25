c0493e9fb2ad50b3452bdda2ee68e6ba3827a50d
YetAnotherDev <oferriot@gmail.com>
2021-11-25 09:10:32 +0100

    Interlosers: module Youtube

--- server/modules/youtube.js
+++ server/modules/youtube.js
@@ -15,52 +15,56 @@ module.exports = { handleYoutube: handleYoutube }
  */
 function handleYoutube(io, message)
 {
-	// Passe le message en minuscules (recherche insensible à la casse)
-	messageMinified = message.toLowerCase();
-	// Recupere le code de la video Youtube
-	var youtubeSearch = message.substring("3");
+    // Passe le message en minuscules (recherche insensible à la casse)
+    messageMinified = message.toLowerCase();
+    // Recupere le code de la video Youtube
+    var youtubeSearch = message.substring("3");
 
-	// Cle API Youtube
-	const ytApiKey = 'AIzaSyB01svjOPmEwl-gwCWEqSXhGWE2JxhN20E';
+    // Cle API Youtube
+    const ytApiKey = 'AIzaSyB01svjOPmEwl-gwCWEqSXhGWE2JxhN20E';
 
-	var options = { maxResults: 5, key: ytApiKey };
-	
-	if (messageMinified.startsWith('yt:'))
-	{
-		search(youtubeSearch, options, function(err, results)
-		{
-			if(err)
-			{
-				// et on affiche un message a glandu qui demqnde de la merde
-				io.sockets.emit('new_message',
-				{
-					name:'Youtube',
-					message:'Ta recherche et pue la défaite.'
-				});
+    var options =
+    {
+        maxResults: 5,
+        key: ytApiKey
+    };
 
-				// on console.log l'erreur
-				return console.log(err);
-			}
-						
-			var ulStart = '<ul>';
-			var ulEnd = '</ul>';
-			var liStart = '<li>';
-			var liEnd = '</li>';
-			var list =  '';
-			results.forEach(result =>
-			{
-				//console.log(result);
-				list += liStart + '<button class="yt-choice" data-id="' + result['id'] + '"type="button">' + result['title'].substring(0, 30) + '</button>' + liEnd;
-				console.log(list);
-			});
+    if (messageMinified.startsWith('yt:'))
+    {
+        search(youtubeSearch, options, function(err, results)
+        {
+            if (err)
+            {
+                // et on affiche un message a glandu qui demqnde de la merde
+                io.sockets.emit('new_message',
+                {
+                    name: 'Youtube',
+                    message: 'Ta recherche et pue la défaite.'
+                });
 
+                // on console.log l'erreur
+                return console.log(err);
+            }
 
-			//results.forEach(result => result['title'].substring(0, 30) );
+            var list = '<ul>';
+            results.forEach(result =>
+            {
+                //console.log(result);
+                list +=
+                    `<li>
+						<button class="yt-choice" data-id="` + result['id'] + `"type="button">` +
+                    result['title'].substring(0, 30) +
+                    `</button>
+					</li>`;
+                console.log(list);
+            });
+            list += '</ul>';
 
-			io.sockets.emit('new_message',
-			{
-				name:'Youtube', message:ulStart + list + ulEnd
-			});
-		});
-	}
+            io.sockets.emit('new_message',
+            {
+                name: 'Youtube',
+                message: list
+            });
+        });
+    }
 }
