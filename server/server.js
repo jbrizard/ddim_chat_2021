12b792cb0db035cb5ca52748bc00bfeb53a18c38
Lucas Michon <wagashisakatsuki@gmail.com>
2021-12-14 11:48:39 +0100

    Ajout du module Sondage / Poll

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var fs = require('fs');			// Accès au système de fichier
 
 // Chargement des modules perso
 var daffy = require('./modules/daffy.js');
+var poll = require('./modules/poll.js');
 
 // Initialisation du serveur HTTP
 var app = express();
@@ -24,6 +25,18 @@ app.get('/', function(req, res)
   
 // Traitement des fichiers "statiques" situés dans le dossier <assets> qui contient css, js, images...
 app.use(express.static(path.resolve(__dirname + '/../client/assets')));
+
+const pollAnswer = 
+{
+	"0": {
+		votes : 0,
+		label : "Oui"
+	},
+	"1": {
+		votes : 0,
+		label : "Non"
+	}
+};
 
 // Gestion des connexions au socket
 io.sockets.on('connection', function(socket)
@@ -46,12 +59,29 @@ io.sockets.on('connection', function(socket)
 		
 		// Transmet le message au module Daffy (on lui passe aussi l'objet "io" pour qu'il puisse envoyer des messages)
 		daffy.handleDaffy(io, message);
+
+		poll.handlePoll(io, message);
 	});
 
 	// Réception du code Konami.
 	socket.on("konami", function()
 	{
 		io.sockets.emit('all_konami');
+	});
+
+	// On receptionne le vote
+	socket.on("vote", (index) => {
+		
+		// Si on vote pour un des résulats, on lui ajoute +1
+		if (pollAnswer[index]) 
+		{
+			pollAnswer[index].votes += 1;
+		}
+
+		console.log(pollAnswer);
+		
+		// On actualise les résultats
+		io.emit("update", pollAnswer);
 	});
 });
 
